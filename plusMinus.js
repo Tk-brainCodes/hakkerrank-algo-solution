f3001adffd2bf31c12a4de69a3d8891eaa0f9bc8
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-17 23:09:09 +0100

    plusMinus algorithm

---
+++ plusMinus.js
@@ -0,0 +1,7 @@
+/*
+Count the total number of positive elements in the array.
+Count the total number of negative elements in the array.
+Count the total number of zero elements in the array.
+Divide the total number of positive elements, negative elements, and zero elements by the size of the array, to get the ratio.
+Print the ratio of positive, negative, and zero elements in the array up to four decimal places.
+*/
