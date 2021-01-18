e1dff2a856b1244c2df93925b39a4960f9a156f1
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-18 09:56:09 +0100

    finished code

--- plusMinus.js
+++ plusMinus.js
@@ -1,7 +1,29 @@
 /*
-Count the total number of positive elements in the array.
-Count the total number of negative elements in the array.
-Count the total number of zero elements in the array.
-Divide the total number of positive elements, negative elements, and zero elements by the size of the array, to get the ratio.
-Print the ratio of positive, negative, and zero elements in the array up to four decimal places.
+1.Count the total number of positive elements in the array.
+2.Count the total number of negative elements in the array.
+3.Count the total number of zero elements in the array.
+4.Divide the total number of positive elements, negative elements, and zero elements by the size of the array, to get the ratio.
+5.Print the ratio of positive, negative, and zero elements in the array up to four decimal places.
 */
+
+
+function plusMinus(arr) {
+    let len = arr.length;
+    //Step 1. 4. 5
+    const getPositiveArray = () => {
+        return arr.filter(item => item > 0).length / len;
+    }
+    //Step 2. 4. 5
+    console.log(getPositiveArray().toFixed(6));
+    const getNegativeArray = () => {
+        return arr.filter(item => item < 0).length / len;
+    }
+    //Step 3. 4. 5
+    console.log(getNegativeArray().toFixed(6));
+    const getZeroArray = () => {
+        return arr.filter(item => item == 0).length / len;
+    }
+    console.log(getZeroArray().toFixed(6));
+}
+
+plusMinus([-4, 3, -9, 0, 4, 1]);
